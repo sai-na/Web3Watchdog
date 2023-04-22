f061b24c9ed151a582daf84cc67cd1e44017db66
sainath378 <92171156+sai-na@users.noreply.github.com>
2023-04-23 02:43:59 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
       <img src={imgUrl} alt="fund_logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
     )}
   </div>
-)
+);
 
 const Sidebar = () => {
   const navigate = useNavigate();
@@ -27,12 +27,12 @@ const Sidebar = () => {
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
         <div className="flex flex-col justify-center items-center gap-3">
           {navlinks.map((link) => (
-            <Icon 
+            <Icon
               key={link.name}
               {...link}
               isActive={isActive}
               handleClick={() => {
-                if(!link.disabled) {
+                if (!link.disabled) {
                   setIsActive(link.name);
                   navigate(link.link);
                 }
@@ -44,7 +44,7 @@ const Sidebar = () => {
         <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
