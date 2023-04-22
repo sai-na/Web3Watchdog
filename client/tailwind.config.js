83ba65889878ad32fa4f030f6cbf79e16bc479b9
sainath378 <92171156+sai-na@users.noreply.github.com>
2023-04-23 00:50:40 +0530

    tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        epilogue: ['Epilogue', 'sans-serif'],
+      },
+      boxShadow: {
+        secondary: '10px 10px 20px rgba(2, 2, 2, 0.25)',
+      },
+    },
   },
   plugins: [],
 };
