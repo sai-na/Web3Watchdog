c534285e40a9f2ae53ed81478ca9a8e066dd6abd
revand5467 <revandsunil5467@gmail.com>
2023-04-22 18:11:12 +0530

    1.5

--- web3/soberhaven/contracts/Contract.sol
+++ web3/soberhaven/contracts/Contract.sol
@@ -7,27 +7,28 @@ contract SoberHaven {
         string title;
         string description;
         uint256 upvotes;
-        uint256 downvotes;
         string location ;
         string image;
         uint256 time;
-        bool show;
+        bool showPublic;
+        bool showPolice;
         address [] voters;
     }
     mapping(uint256 => Post) public posts;
     uint256 public postCount = 0;
-function createPost(address _owner , string memory _title, string memory _description, uint256 _upvotes, uint256 _downvotes, string memory _location , string memory _image, uint256 _time , bool _show) public returns(uint256){
+    address[] public police = [0x936F3348c3035ea5530F0d959272DC6cC0402C44,0x936F3348c3035ea5530F0d959272DC6cC0402C44];
+function createPost(address _owner , string memory _title, string memory _description, uint256 _upvotes, string memory _location , string memory _image, uint256 _time ) public returns(uint256){
     Post storage post = posts[postCount];
     require( post.time <= block.timestamp, "Time must be lesser  than current time");
     post.owner = _owner;
     post.description = _description;
     post.upvotes = _upvotes;
-    post.downvotes = _downvotes;
     post.location = _location;
     post.image = _image;
     post.time = _time;
     post.title=_title;
-    post.show = true;
+    post.showPublic = false;
+    post.showPolice= false;
     postCount++;
     
     return postCount -1;
@@ -42,7 +43,17 @@ function getPosts() public view returns(Post[] memory){
    }
    return allPosts;
 }
-function visibility( uint256 _id , address police) public returns (bool) {
+function updatePublicView( uint256 _id ) public returns (bool) {
+    //require(msg.sender == police, "Only police can update the view");
+    for(uint i=0 ; i<police.length ; i++){
+        if(msg.sender == police[i]){
+            Post storage post = posts[_id];
+            post.showPolice = true;
+            return true;
+        }
+    }
+   
     
+    return false;
 }
 }
