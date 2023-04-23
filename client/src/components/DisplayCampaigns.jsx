81caa19b25224635b180580d44ab8d880ea04486
sainath378 <92171156+sai-na@users.noreply.github.com>
2023-04-24 00:38:42 +0530

    cards

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -8,24 +8,42 @@ import Card from './Card';
 
 
 const DisplayCampaigns = ({ title, isLoading, posts }) => {
-const [sortedPosts, setSortedPosts]  = useState([]);
+  const [sortedPosts, setSortedPosts] = useState([]);
   const navigate = useNavigate();
-  useEffect(()=>{
-    let temp =[]
-    let s =""
-   if(posts){ posts.map((post,index) => 
-    s=new Date(d).getTime(),
-    temp.push ({...post , postTime:s}) 
-),
-setSortedPosts(temp)}},[posts])
+
+
+
+
+
+  posts = posts.filter((item, index) => {
+    // Keep all items except for the one with title 'sge4g w4twt'
+    return item.title !== 'Et sed id ullam cul';
+  });
+
+  useEffect(() => {
+
+    //console.log(temp);
+
+
+
+    let postsWithTimestamps = posts?.map(post => {
+      const postTime = (new Date((parseInt(post?.postTime.toString())) * 1000));;
+      const eventTime = (new Date((parseInt(post?.eventTime.toString())) * 1000));;
+
+
+      return { ...post, postTime, eventTime };
+    });
+    postsWithTimestamps = postsWithTimestamps.sort((a, b) => b.postTime - a.postTime);
+    setSortedPosts(postsWithTimestamps);
+  }, [posts]);
 
 
   const handleNavigate = (post) => {
-    navigate(`/post-details/${post.title}`, { state: post })
-  }
+    navigate(`/post-details/${post.title}`, { state: post });
+  };
 
-  console.log(sortedPosts)
-  
+  console.log(sortedPosts);
+
   return (
     <div>
       <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">{title} ({posts?.length})</h1>
@@ -35,20 +53,20 @@ setSortedPosts(temp)}},[posts])
           <img src={loader} alt="loader" className="w-[100px] h-[100px] object-contain" />
         )}
 
-        {!isLoading && posts?.length === 0 && (
+        {!isLoading && sortedPosts?.length === 0 && (
           <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
             You have not created any campigns yet
           </p>
         )}
 
-        {!isLoading && posts?.length > 0 && posts?.map((post,index) => <Card
+        {!isLoading && sortedPosts?.length > 0 && sortedPosts?.map((post, index) => <Card
           key={index}
           {...post}
           handleClick={() => handleNavigate(post)}
         />)}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DisplayCampaigns
+export default DisplayCampaigns;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 import { DisplayCampaigns } from '../components';
-import { useStateContext } from '../context'
+import { useStateContext } from '../context';
 import Card from '../components/Card';
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [campaigns, setCampaigns] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   const { address, contract, getAdminPost } = useStateContext();
 
   const fetchCampaigns = async () => {
     setIsLoading(true);
     const data = await getAdminPost();
-    setCampaigns(data);
+    setPosts(data);
     setIsLoading(false);
-  }
+  };
 
   useEffect(() => {
-    if(contract) fetchCampaigns();
+    if (contract) fetchCampaigns();
   }, [address, contract]);
 
   return (
-   
-    <DisplayCampaigns 
+
+    <DisplayCampaigns
       title="All Campaigns"
       isLoading={isLoading}
-      campaigns={campaigns}
+      posts={posts}
     />
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
